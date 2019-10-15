f3224d4919bee857e1002cce137b867e986c77d8
Will <w.stolton@gmail.com>
2019-10-15 21:27:14 +0100

    updated

--- src/EmpiricalAnalysis.java
+++ src/EmpiricalAnalysis.java
@@ -2,7 +2,7 @@ public class EmpiricalAnalysis {
 
     public static void main (String[] args) {
 
-        Integer[] number = new Integer[] {50,500,5000,50000,500000};
+        Integer[] number = new Integer[] {50,500,5000,50000,500000};    //my preferred method to running them all individually.
         for (int num : number) {
             long start = System.nanoTime();
             CreateArray ca = new CreateArray();
